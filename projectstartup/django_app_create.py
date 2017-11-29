9fa10878335bfab8c07a386c22a2056ce2e4a395
Shota Shimazu <hornet.live.mf@gmail.com>
2017-11-29 23:37:02 +0900

    add readme creator

--- projectstartup/django_app_create.py
+++ projectstartup/django_app_create.py
@@ -2,13 +2,18 @@ from console import command
 from os import chdir
 from shutil import move
 from console import log
+from projectstartup.readme import create_readme_doc
 
 def dj_new_flow(name = None):
     if name == None:
         log("Please type your new Django application name.")
         name = log("App Name", withInput = True)
+
     _create_new_django_app(name)
+
+    chdir("./" + name)
     _create_template_git_project(name)
+    _create_docs(name)
 
 
 def _create_new_django_app(name):
@@ -16,6 +21,10 @@ def _create_new_django_app(name):
 
 
 def _create_template_git_project(name):
-    chdir("./" + name)
     command("curl -O https://raw.githubusercontent.com/github/gitignore/master/Python.gitignore")
     move("Python.gitignore", ".gitignore")
+    command("git init")
+
+def _create_docs(name):
+    with open("README.md", "a") as readme:
+        readme.write(create_readme_doc(name))
---
+++ projectstartup/readme.py
@@ -0,0 +1,16 @@
+import textwrap
+
+def create_readme_doc(name):
+
+    django_version = "no_version"
+
+    return textwrap.dedent(
+'''
+# {name}
+This is your first Django application.
+
+# Info
+
+## Django Version
+{django_version}
+''').format(name=name, django_version=django_version).strip()
