a828f1d466fd6e4701636f7b97d0797bd1dbe1fa
Shota Shimazu <hornet.live.mf@gmail.com>
2017-11-30 13:25:03 +0900

    update setup script

--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
-from setuptools import setup
+# -*- encoding:utf-8 -*-
+from setuptools import setup, find_packages
 
 setup(
+    name = "django-console",
+    version = "0.0.1",
+    author = "Shota Shimazu",
+    author_email = "hornet.live.mf@gmail.com",
+    packages = find_packages(),
+    install_requires=[
+        "django",
+    ],
+    description = "Advanced Django comsole",
+    long_description = "Advanced Django command line tools like Rails.",
+    url = "https://github.com/shotastage/django-console/",
+    license = "Apache",
+    platforms = ["POSIX", "Windows", "Mac OS X"],
     entry_points={
-        "console_scripts": [
-            "django-console=django-console.__main__:main"
-        ]
+        'console_scripts': 'django = django-console:main'
     },
+    zip_safe=False,
+    classifiers=[
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Utilities'
+    ]
 )
