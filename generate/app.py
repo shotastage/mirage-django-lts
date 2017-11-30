424d74964aa8801733cc0a9eda5663ce40cadaae
Shota Shimazu <hornet.live.mf@gmail.com>
2017-11-30 11:45:26 +0900

    add url adder

--- generate/app.py
+++ generate/app.py
@@ -1,5 +1,6 @@
 import os
-from console import log
+from generate.urlpy import create_urlpy_script as url_script
+from console import log, command
 
 
 def dj_app_flow(third_args):
@@ -9,6 +10,20 @@ def dj_app_flow(third_args):
 
     for app in third_args:
         log("Creating a app " + app + ".")
+        _create_app(app)
+        _create_url(app)
+
+
+def _create_app(name):
+    command("python manage.py startapp " + name)
+
+def _create_url(name):
+    current = os.getcwd()
+    os.chdir("./" + name)
+    with open("urls.py", "a") as newscript:
+        newscript.write(url_script(name))
+    os.chdir(current)
+
 
 def _check_all(third_args):
     status = True
---
+++ generate/urlpy.py
@@ -0,0 +1,16 @@
+import textwrap
+
+def create_urlpy_script(app):
+
+    django_version = "no_version"
+
+    return textwrap.dedent(
+'''
+from django.conf.urls import url
+from {app}.views import YOUR_VIEW_CLASS
+
+urlpatterns = [
+    url(r'^url_letter/', YOUR_VIEW_CLASS.as_view(), name='starts'),
+]
+
+''').format(app=app).strip()
