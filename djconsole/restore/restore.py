2adba653d3032b814ea482fbacefc5a333807146
Shota Shimazu <hornet.live.mf@gmail.com>
2017-12-04 10:30:53 +0900

    add restore flow class

---
+++ djconsole/restore/restore.py
@@ -0,0 +1,14 @@
+from djconsole.flow import Flow
+
+class DjangoRestoreFlow(Flow):
+  
+  def __init__(self):
+    pass
+  
+  def flow(self):
+    pass
+  
+  
+  def _check_backup_existence(self):
+    pass
+  
