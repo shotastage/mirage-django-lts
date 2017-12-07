00b271de933c15e1a132b615218c079a03838dd5
Shota Shimazu <hornet.live.mf@gmail.com>
2017-12-07 22:13:10 +0900

    fix import

--- djconsole/cms_projectstartup/django_cms_create.py
+++ djconsole/cms_projectstartup/django_cms_create.py
@@ -1,10 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright 2017 Shota Shimazu.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
 import os
 
-from djconsole.flow import Flow
-
-from os import getcwd, chdir, path
-from djconsole.command import log, command
 from shutil import move
+from djconsole.flow import Flow
+from djconsole.command import log, command
 from djconsole.cms_projectstartup.readme import create_readme_doc
 
 
@@ -28,11 +43,11 @@ class DjangoCMSStartupFlow(Flow):
         
         self._create_new_django_app(self._project_name)
 
-        current = getcwd()
-        chdir("./" + self._project_name)
+        current = os.getcwd()
+        os.chdir("./" + self._project_name)
         self._create_template_git_project(self._project_name)
         self._create_docs(self._project_name)
-        chdir("../")
+        os.chdir("../")
 
 
     def _create_new_django_app(self, name):
@@ -52,5 +67,5 @@ class DjangoCMSStartupFlow(Flow):
 
 
     def _check(self, name):
-        if path.exists(name):
+        if os.path.exists(name):
             raise FileExistsError
--- djconsole/destroy/app.py
+++ djconsole/destroy/app.py
@@ -1,13 +1,30 @@
-import shutil
-from os import path
-import tarfile
+# -*- coding: utf-8 -*-
+"""
+Copyright 2017 Shota Shimazu.
 
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+
+import shutil
+import tarfile
+import os
 from djconsole.command import log
 from djconsole.backup import backup
 
 
 def _check(name):
-    if path.isdir(name):
+    if os.path.isdir(name):
         return
     else:
         raise FileNotFoundError
