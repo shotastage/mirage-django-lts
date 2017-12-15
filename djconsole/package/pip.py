bcbbf236c581dedee7432842ad9de6e6580a61ff
Shota Shimazu <hornet.live.mf@gmail.com>
2017-12-15 14:12:23 +0900

    add dj packager

---
+++ djconsole/package/pip.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright 2017 Shota Shimazu.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+import os
+
+from djconsole.flow import Flow
+from djconsole.command import log
+from djconsole.command import command
+
+
+class DjangoPackageFlow(Flow):
+
+    def __init__(self, subcommand):
+        log("Django Console Packager is now unser development!", withError = True)
+        self._subcommand = subcommand
+
+    def flow(self):
+        if self._subcommand == "check":
+            self._check()
+        else:
+            log("Unkown command " + self._subcommand + "!", withError = True)
+
+    def _check(self):
+        os.system("pip list -o")
+
+    def _puash(self):
+        command("git push -u origin master")
