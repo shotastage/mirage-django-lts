fcd108a442c2298634ff36c8aa9650d2d9d74a9c
Shota Shimazu <hornet.live.mf@gmail.com>
2018-01-15 10:56:29 +0900

    up version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ sys.path.append('./tests')
 if __name__ == "__main__":
     setup(
         name = "djconsole",
-        version='0.0.10',
+        version='0.0.11',
         author = "Shota Shimazu",
         author_email = "hornet.live.mf@gmail.com",
         packages = find_packages(),
