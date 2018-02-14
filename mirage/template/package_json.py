58144c76e34b8b2aadc817e37cff23925d4b0ae7
Shota Shimazu <hornet.live.mf@gmail.com>
2018-02-15 02:43:31 +0900

    add template

---
+++ mirage/template/package_json.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright 2017-2018 Shota Shimazu.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+import textwrap
+
+def src(proj_name, ver, desc, git_url, author, author_email, license_name):
+
+    return "{\n" + textwrap.dedent(
+'''
+    "name": "{PROJECT_NAME}-shell",
+    "version": "{VERSION}",
+    "description": "{DESCRIPTION}",
+    "main": "index.js",
+    "repository": "{GIT_REPOSITORY}",
+    "author": "{AUTHOR_NAME} <{EMAIL}>",
+    "license": "{LICENSE}"
+''').format(
+        PROJECT_NAME = proj_name,
+        VERSION = ver,
+        DESCRIPTION = desc,
+        GIT_REPOSITORY = git_url,
+        AUTHOR_NAME = author,
+        EMAIL = author_email,
+        LICENSE = license_name
+    ).strip() + "\n}"
