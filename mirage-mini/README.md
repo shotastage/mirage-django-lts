b70475c5aca99bbf714907611a3bc24629bbd66b
Shota Shimazu <hornet.live.mf@gmail.com>
2018-02-22 22:51:52 +0900

    remove unused commands

--- mirage-mini/README.md
+++ mirage-mini/README.md
@@ -1,4 +1,4 @@
-Mirage Mini
+# Mirage Mini
 
 Mirage mini is CLI for your Django Web Application. It provides general console to manage server like Mirage CLI.
 
@@ -9,12 +9,16 @@ Mirage mini is CLI for your Django Web Application. It provides general console 
 
 To install Mirage mini, run this command in your Django application root. It requires Miragefile configuration that you have to configure it before. If the file is not found, this installation will be failed.
 
+```
 mi extend bin
- 
+```
+
 # Usage
 
-- <your_app_bin> start
+```
+<your_app_bin> start
 
-- <your_app_bin> restart
+<your_app_bin> restart
 
-- <your_app_bin> stop
+<your_app_bin> stop
+```
--- mirage-mini/mirage.py
+++ mirage-mini/mirage.py
@@ -30,37 +30,13 @@ def main():
     parser.add_argument("v", "version", None, "VersionShow")
 
     # Commands
-    parser.add_argument("new", "newproject", None, "DjangoStartup")
-    parser.add_argument_with_subaction("new", "newproject", "react", None, "ReactStartup")
-    parser.add_argument_with_subaction("new", "newproject", "cms", None, "DjangoCMSStartup")
-
-    parser.add_argument("b", "backup", "app", "DjangoBackupApp")
-
-    parser.add_argument("conf", "configure", None, "Configure")
-
     parser.add_argument("c", "console", None, "DjangoConsole")
     parser.add_argument_with_subaction("c", "console", "db", None, "DjangoDBConsole")
-
-    parser.add_argument("d", "destroy", "app", "DjangoDestroy")
 
     parser.add_argument_with_subaction("db", "database", "migrate", None, "DjangoMigrate")
     parser.add_argument_with_subaction("db", "database", "reset", None, "DjangoDBReset")
 
-    # parser.add_argument("d", "destroy", None, "DjangoDestroy")
-
-    parser.add_argument("g", "generate", "app", "DjangoAppMake")
-
-    parser.add_argument("g", "generate", "model", "DjangoModelMake")
-
-    parser.add_argument("heroku", "heroku_util", "configure", "DjangoHerokuConfigure")
-
-    parser.add_argument("ide", "scaffold", None, "Scaffold")
-    parser.add_argument("internal_server_launch", "internal_server_launch", None, "ScaffoldServer")
-    parser.add_argument("internal_debug_server_launch", "internal_debug_server_launch", None, "ScaffoldDebugServer")
-
     parser.add_argument("s", "server", None, "DjangoDebugServer")
-
-    parser.add_argument("f", "file", None, "Touch")
 
     # Excute
     parser.parse()
