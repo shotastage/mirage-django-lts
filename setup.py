f937545de93a4db47c6ec2c818dd2a907496f75a
Shota Shimazu <hornet.live.mf@gmail.com>
2018-03-04 14:47:10 +0900

    upgrade version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ sys.path.append('./tests')
 if __name__ == "__main__":
     setup(
         name = "django-mirage",
-        version = "0.0.23",
+        version = "0.0.24",
         author = "Shota Shimazu",
         author_email = "hornet.live.mf@gmail.com",
         classifiers=[
