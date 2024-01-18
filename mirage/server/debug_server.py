c1b5b7d07263464035f4ad4ecdef6d3f833aa959
shotastage <shota.shimazu@magicalsoft.app>
2024-01-18 20:54:13 +0900

    revise

--- mirage/server/debug_server.py
+++ mirage/server/debug_server.py
@@ -1,30 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Copyright 2017-2018 Shota Shimazu.
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-"""
-
 import os
 import webbrowser
 from mirage import proj
 from mirage.flow import Workflow
+from mirage.flow_next import WorkflowNext
 from mirage import system as mys
 
 
 class DjangoDebugServerWorkFlow(Workflow):
-    
-    def main(self):        
+
+    def main(self):
         with proj.MirageEnvironment(proj.MirageEnvironmentLevel.indjango):
             try:
                 os.system("python manage.py runserver")
@@ -35,9 +19,9 @@ class DjangoDebugServerWorkFlow(Workflow):
 
 
 
-class DjangoLaunchBrowserWorkflow(Workflow):
+class DjangoLaunchBrowserWorkflow(WorkflowNext):
 
-    def constructor(self):
+    def __init__(self):
         self._url = self._option
 
     def main(self):
