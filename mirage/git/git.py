e2b43f9b69001e8c4b37b0074ec34c230713571e
shotastage <shota.shimazu@magicalsoft.app>
2024-01-18 23:29:11 +0900

    add git workflow

--- mirage/git/git.py
+++ mirage/git/git.py
@@ -1,38 +1,27 @@
-# -*- coding: utf-8 -*-
-"""
-Copyright 2017 Shota Shimazu.
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-"""
-
 import os
 
 from mirage.flow import Flow
 from mirage.command import log
 from mirage.command import command
+from mirage import system as mys
 
 
-class DjangoGitFlow(Flow):
+class DjangoGitPullFlow(Flow):
 
     def __init__(self, subcommand):
         log("Django Console Git is now unser development!", withError = True)
-        self._subcommand = subcommand
 
     def flow(self):
-        os.system("git " + self._subcommand)
+        command("git pull origin main")
 
-    def _pull(self):
-        command("git pull origin master")
 
-    def _puash(self):
-        command("git push -u origin master")
+class DjangoGitPushFlow(Flow):
+
+    def __init__(self, subcommand):
+        log("Django Console Git is now unser development!", withError = True)
+
+    def flow(self):
+        commit_message = mys.log("Commit message: ", withInput = True, default = "update code base")
+        command("git add .")
+        command("git commit -m " + commit_message)
+        command("git push -u origin main")
--- mirage/mirage.py
+++ mirage/mirage.py
@@ -1,19 +1,3 @@
-"""
-Copyright 2017-2023 Shota Shimazu.
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-"""
-
 from mirage.djargparse import ArgumentsParser
 
 
@@ -52,6 +36,10 @@ def main():
     parser.add_argument("g", "generate", "module", "ModuleCreateFlow")
 
     parser.add_argument("heroku", "heroku", "configure", "DjangoHerokuConfigure")
+
+    parser.add_argument("git", "git", "pull", "DjangoGitPull")
+
+    parser.add_argument("git", "git", "push", "DjangoGitPush")
 
     parser.add_argument("m", "manage", None, "DjangoManagePy")
 
